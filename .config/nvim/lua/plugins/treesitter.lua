f824d198f4599f3fe11142cafad65f23677cbd43
James Svacha <jbsvacha@gmail.com>
2024-04-19 16:55:50 -0400

    Fixed typo

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,4 +3,9 @@ return {
     dependencies = {
         'nvim-treesitter/nvim-treesitter-context',
     },
+    config = function()
+        vim.keymap.set("n", "[c", function()
+            require("treesitter-context").go_to_context(vim.v.count1)
+        end, { silent = true })
+    end
 }
