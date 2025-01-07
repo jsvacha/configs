4aa5facd8207545afe847a25980fa88022c1897c
James Svacha <jbsvacha@gmail.com>
2025-01-06 19:51:28 -0500

    Get completions working

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -22,10 +22,6 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
-        dependencies = {
-            "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-nvim-lsp",
-        },
         config = function()
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
